d03a8f2cb4ab1160b6e91b4b7d10774483400e2e
Jonas Vestgarden <jonasmohammed@hotmail.no>
2018-02-05 15:10:19 +0100

    Oblig 2

---
+++ Oblig2/Oblig2/Views/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using Windows.UI.Xaml.Controls;
+
+namespace Oblig2.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private static DirectoryInfo directorySelected;
+        private static string directoryPath = @"C:\compress";
+
+        private static void Compress(DirectoryInfo directorySelected)
+        {
+            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+            {
+                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                {
+                    if ((File.GetAttributes(fileToCompress.FullName) &
+                       FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                    {
+                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                        {
+                            using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                               CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
+
+                            }
+                        }
+                        FileInfo info = new FileInfo(directoryPath + "\\" + fileToCompress.Name + ".gz");
+                        Debug.WriteLine("Compressed {0} from {1} to {2} bytes.",
+                        fileToCompress.Name, fileToCompress.Length.ToString(), info.Length.ToString());
+                    }
+
+                }
+            }
+        }
+
+        private static void Decompress(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Debug.WriteLine("Decompressed: {0}", fileToDecompress.Name);
+                    }
+                }
+            }
+        }
+
+        private void CompressBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            directorySelected = new DirectoryInfo(directoryPath);
+            Compress(directorySelected);
+        }
+
+        private void DeCompressBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            foreach(FileInfo fileToDecompress in directorySelected.GetFiles("*.gz"))
+            {
+                Decompress(fileToDecompress);
+            }
+        }
+    }
+}
