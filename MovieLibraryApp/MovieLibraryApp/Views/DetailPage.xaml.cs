a7c97e5ddb5fbf665874508db744cdc2fc20eeda
Jonas Vestgarden <jonasmohammed@hotmail.no>
2018-04-10 04:45:51 +0200

    Started working on the final project

---
+++ MovieLibraryApp/MovieLibraryApp/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MovieLibraryApp.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace MovieLibraryApp.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
